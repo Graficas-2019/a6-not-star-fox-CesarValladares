48b6c9c7e39fefacd93bcd1cbfb3d121c9ea2788
CesarValladares <a01023506@itesm.mx>
2019-03-26 15:48:39 -0600

    26/marz

--- notStarFox.js
+++ notStarFox.js
@@ -5,8 +5,9 @@ camera = null,
 root = null,
 group = null,
 ship = null,
+building= null,
 directionalLight = null;
-//orbitControls = null;
+orbitControls = null;
 var object;
 var animator = null;
 loopAnimation = false;
@@ -22,16 +23,19 @@ var velocity = new THREE.Vector3();
 var direction = new THREE.Vector3();
 
 var controls = null;
-
 var keyboard = null;
+
+var buildings = [];
 
 var ship_loaded = 0;
 var bool = true;
 
 var objLoader = null;
+var mtlLoader = null;
 
 var duration = 1500; // ms
 var currentTime = Date.now();
+var actualTime = Date.now();
 
 var score = 0; 
 
@@ -42,18 +46,37 @@ function animate() {
     var delta = now - currentTime;
     currentTime = now;
 
-    //velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+    seconds = (now - actualTime)/1000
+    
+    if (seconds >= 3 ){
+ 
+        cloneObj(); 
+        actualTime = now;
+    }
+    
     direction.y = Number( moveForward ) - Number( moveBackward );
-    direction.x = Number( moveLeft ) - Number( moveRight );
+    direction.z = Number( moveLeft ) - Number( moveRight );
     direction.normalize(); // this ensures consistent movements in all directions
-    if ( moveForward || moveBackward ) velocity.z -=  1.0 * delta;
-    if ( moveLeft || moveRight ) velocity.y -=  1.0 * delta;
+    if ( moveForward || moveBackward ) velocity.y += direction.y *0.005* delta;
+    if ( moveLeft || moveRight ) velocity.z += direction.z *0.005* delta;
     
     controls.getObject().translateY( velocity.y * delta );
     controls.getObject().translateZ( velocity.z * delta );
 
-    console.log(ship.position)
+    velocity.y = 0;
+    velocity.z = 0;
 
+    for(building_i of buildings){
+
+        building_i.position.x += 1;
+        
+        if (building_i.position.x >=300){
+
+            scene.remove(building_i)
+
+        }
+    }
+    
 }
 
 function run()
@@ -72,61 +95,214 @@ function run()
         //orbitControls.update();
 }
 
+function cloneObj(){
+
+    var newBuilding = building.clone();
+
+    zPos = Math.floor(Math.random() * 200) - 100  
+
+    newBuilding.position.z = zPos;
+    newBuilding.position.x = -250;
+    newBuilding.position.y = 0;
+
+    scene.add(newBuilding);
+    buildings.push(newBuilding);
+
+}
+
 function loadObj()
 {
-    if(!objLoader)
 
-        objLoader = new THREE.OBJLoader();
+    if(!mtlLoader)
 
-    objLoader.load(
-        'models/Arwing/Arwing_001.obj',
+        mtlLoader = new THREE.MTLLoader();
 
-        function(object)
-        {
-            //var texture = new THREE.TextureLoader().load('models/Tie_Fighter/texture.jpg');
-            //var normalMap = new THREE.TextureLoader().load('Stanford_Bunny_OBJ-JPG/bunnystanford_res1_UVmapping3072_TerraCotta_g001c.jpg');       
-            object.traverse( function ( child ) 
-            {
-                if ( child instanceof THREE.Mesh ) 
+    mtlLoader.load(
+        'models/falcon/millenium-falcon.mtl',
+        
+        function(materials){
+
+            materials.preload();
+
+            if(!objLoader)
+
+                objLoader = new THREE.OBJLoader();
+
+                objLoader.setMaterials(materials)
+
+            objLoader.load(
+                'models/falcon/millenium-falcon.obj',
+
+                function(object)
                 {
-                    child.castShadow = true;
-                    child.receiveShadow = true;
-                    //child.material.map = texture;
-                    //child.material.normalMap = normalMap;
-                }
-            } );
+                    var texture = new THREE.TextureLoader().load('models/falcon/falcon.jpg');
+                    //var normalMap = new THREE.TextureLoader().load('Stanford_Bunny_OBJ-JPG/bunnystanford_res1_UVmapping3072_TerraCotta_g001c.jpg');       
+                    object.traverse( function ( child ) 
+                    {
+                        if ( child instanceof THREE.Mesh ) 
+                        {
+                            child.castShadow = true;
+                            child.receiveShadow = true;
+                            child.material.map = texture;
+                            //child.material.normalMap = normalMap;
+                        }
+                    } );
+                            
+                    ship = object;
+                    ship.scale.set(0.03,0.03,0.03);
+                    ship.position.z = 0;
+                    ship.position.x = 350;
+                    ship.position.y = 50;
+                    ship.rotation.y = Math.PI /2;
                     
-            ship = object;
-            ship.scale.set(5,5,5);
-            ship.position.z = 0;
-            ship.position.x = 250;
-            ship.position.y = 100;
-            ship.rotation.y = Math.PI /2;
+                    group.add(ship);
+                },
+                function ( xhr ) {
+
+                    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+                    ship_loaded = ( xhr.loaded / xhr.total * 100 )
+
+                    if (ship_loaded >= 100 && bool){
+                        console.log("controls")
+                        controls = new THREE.PointerLockControls(group);
+                        scene.add(controls.getObject());
+
+                        bool = false;
+                    }
             
-            group.add(ship);
-        },
-        function ( xhr ) {
-
-            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-            ship_loaded = ( xhr.loaded / xhr.total * 100 )
-
-            if (ship_loaded >= 100 && bool){
-                console.log("controls")
-                controls = new THREE.PointerLockControls(group);
-                scene.add(controls.getObject());
-
-                bool = false;
-            }
+                },
+                // called when loading has errors
+                function ( error ) {
+            
+                    console.log( 'An error happened' );
+            
+                });
+        } 
+    )
     
-        },
-        // called when loading has errors
-        function ( error ) {
+}
+
+function loadBuilding(){
+
+
+    if(!mtlLoader)
+
+        mtlLoader = new THREE.MTLLoader();
+
+    mtlLoader.load(
+        'models/building2/Building.mtl',
+        
+        function(materials){
+
+            materials.preload();
+
+            if(!objLoader)
+
+                objLoader = new THREE.OBJLoader();
+
+                objLoader.setMaterials(materials)
+
+            objLoader.load(
+                'models/building2/Building.obj',
+
+                function(object)
+                {
+                    //var texture = new THREE.TextureLoader().load('models/Tie_Fighter/texture.jpg');
+                    //var normalMap = new THREE.TextureLoader().load('Stanford_Bunny_OBJ-JPG/bunnystanford_res1_UVmapping3072_TerraCotta_g001c.jpg');       
+                    object.traverse( function ( child ) 
+                    {
+                        if ( child instanceof THREE.Mesh ) 
+                        {
+                            child.castShadow = true;
+                            child.receiveShadow = true;
+                            //child.material.map = texture;
+                            //child.material.normalMap = normalMap;
+                        }
+                    } );
+                            
+                    building = object;
+                    building.scale.set(0.5,0.5,0.5);
+                    building.position.z = 0;
+                    building.position.x = -250;
+                    building.position.y = -150;
+                    building.rotation.y = Math.PI;
+                    
+                    scene.add(building);
+                },
+                function ( xhr ) {
+
+                    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+                    building_loaded = ( xhr.loaded / xhr.total * 100 )
+            
+                },
+                // called when loading has errors
+                function ( error ) {
+            
+                    console.log( 'An error happened' );
+            
+                });
+        } 
+    )
+
+}
+
+function loadEnemyShip(){
+
+    // if(!objLoader)
+
+    //     objLoader = new THREE.OBJLoader();
+
+    // objLoader.load(
+    //     'models/Arwing/Arwing_001.obj',
+
+    //     function(object)
+    //     {
+    //         //var texture = new THREE.TextureLoader().load('models/Tie_Fighter/texture.jpg');
+    //         //var normalMap = new THREE.TextureLoader().load('Stanford_Bunny_OBJ-JPG/bunnystanford_res1_UVmapping3072_TerraCotta_g001c.jpg');       
+    //         object.traverse( function ( child ) 
+    //         {
+    //             if ( child instanceof THREE.Mesh ) 
+    //             {
+    //                 child.castShadow = true;
+    //                 child.receiveShadow = true;
+    //                 //child.material.map = texture;
+    //                 //child.material.normalMap = normalMap;
+    //             }
+    //         } );
+                    
+    //         ship = object;
+    //         ship.scale.set(5,5,5);
+    //         ship.position.z = 0;
+    //         ship.position.x = 250;
+    //         ship.position.y = 50;
+    //         ship.rotation.y = Math.PI /2;
+            
+    //         group.add(ship);
+    //     },
+    //     function ( xhr ) {
+
+    //         console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+    //         ship_loaded = ( xhr.loaded / xhr.total * 100 )
+
+    //         if (ship_loaded >= 100 && bool){
+    //             console.log("controls")
+    //             controls = new THREE.PointerLockControls(group);
+    //             scene.add(controls.getObject());
+
+    //             bool = false;
+    //         }
     
-            console.log( 'An error happened' );
+    //     },
+    //     // called when loading has errors
+    //     function ( error ) {
     
-        });
+    //         console.log( 'An error happened' );
     
+    //     });
+
 }
 
 function createScene(canvas) 
@@ -136,14 +312,14 @@ function createScene(canvas)
     renderer = new THREE.WebGLRenderer( { canvas: canvas, antialias: true } );
 
     // Set the viewport size
-    renderer.setSize(window.innerWidth -10, window.innerHeight -10);
+    renderer.setSize(window.innerWidth -20, window.innerHeight -20);
 
     // Create a new Three.js scene
     scene = new THREE.Scene();
 
     // Add  a camera so we can view the scene
     camera = new THREE.PerspectiveCamera( 45, canvas.width / canvas.height, 1, 4000 );
-    camera.position.set(400,100,0);
+    camera.position.set(400,50,0);
     camera.lookAt(new THREE.Vector3(0,50,0))
     scene.add(camera);
 
@@ -174,7 +350,7 @@ function createScene(canvas)
     var color = 0xffffff;
 
     // Put in a ground plane to show off the lighting
-    geometry = new THREE.PlaneGeometry(500, 1000, 50, 50);
+    geometry = new THREE.PlaneGeometry(600, 1000, 50, 50);
     var mesh = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({color:color, map:map, side:THREE.DoubleSide}));
     mesh.rotation.x = -Math.PI / 2;
 
@@ -188,6 +364,8 @@ function createScene(canvas)
 
     // Load ship
     loadObj();
+
+    loadBuilding();
 
     var onKeyDown = function ( event ) {
         switch ( event.keyCode ) {
@@ -234,6 +412,7 @@ function createScene(canvas)
     window.addEventListener( 'resize', onWindowResize);
     document.addEventListener( 'keydown', onKeyDown, false );
     document.addEventListener( 'keyup', onKeyUp, false );
+
 }
 
 /*function onDocumentKeyDown(event){
